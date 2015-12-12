b11e4df305d45d76c779cd759f846f778c009765
Wakaba <wakaba@suikawiki.org>
2015-12-12 18:32:40 +0900

    cert

--- server.sh
+++ server.sh
@@ -2,4 +2,5 @@
 cp /ngircd.conf.template /ngircd.conf
 sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g' /ngircd.conf
 sed -i 's/@@ADMIN@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
+sed -i 's/@@CERTDIR@@/'$TLS_CERT_PATH'/g' /ngircd.conf
 exec /usr/sbin/ngircd -n -f /ngircd.conf
