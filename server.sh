2718941c95c2e5a51baf3e2ecb10518a42ac4d79
Wakaba <wakaba@suikawiki.org>
2015-12-12 18:03:38 +0900

    typo

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /ngircd.conf.template /ngircd.conf
-sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g'
-sed -i 's/@@ADMIN@@/'$IRC_ADMIN_EMAIL'/g'
+sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g' /ngircd.conf
+sed -i 's/@@ADMIN@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
 exec /usr/sbin/ngircd -n -f /ngircd.conf
