c80bf6d2addfde1ee2a4c0987f82285e8ce9ca18
Wakaba <wakaba@suikawiki.org>
2015-12-12 19:18:40 +0900

    escaping

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /ngircd.conf.template /ngircd.conf
 sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g' /ngircd.conf
-sed -i 's/@@ADMIN@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
-sed -i 's/@@CERTDIR@@/'$TLS_CERT_PATH'/g' /ngircd.conf
+sed -i 's/@@EMAIL@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
+sed -i 's/@@CERTDIR@@/'${TLS_CERT_PATH//\//\\/}'/g' /ngircd.conf
 exec /usr/sbin/ngircd -n -f /ngircd.conf
