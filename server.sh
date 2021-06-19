debee108d56b3a53e92355589eabdc5c5c1315f5
Wakaba <wakaba@suikawiki.org>
2021-06-19 10:32:02 +0900

    clarification

--- server.sh
+++ server.sh
@@ -4,3 +4,8 @@ sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g' /ngircd.conf
 sed -i 's/@@EMAIL@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
 sed -i 's/@@CERTDIR@@/'${TLS_CERT_PATH//\//\\/}'/g' /ngircd.conf
 exec /usr/sbin/ngircd -n -f /ngircd.conf
+
+## Per CC0 <https://creativecommons.org/publicdomain/zero/1.0/>, to
+## the extent possible under law, the author of the files in this
+## repository has waived all copyright and related or neighboring
+## rights to the files in this repository.
