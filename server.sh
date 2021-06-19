a42e6473a0617fc918442fe3f11f63bd5efb9ad9
Wakaba <wakaba@suikawiki.org>
2021-06-19 11:09:10 +0900

    Allow missing of conf.d

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ cp ${IRC_CONF_TEMPLATE:-/ngircd.conf.template} /ngircd.conf
 
 confd=${IRC_CONF_DIR:-/tmp/ngircd.conf.d}
 mkdir -p $confd
-cat $confd/*.conf >> /ngircd.conf
+cat $confd/*.conf >> /ngircd.conf || true
 
 sed -i 's/@@DOMAIN@@/'$IRC_HOSTNAME'/g' /ngircd.conf
 sed -i 's/@@EMAIL@@/'$IRC_ADMIN_EMAIL'/g' /ngircd.conf
